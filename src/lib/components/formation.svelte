b4ea58a987a14f89e5a63213a407339b8bb90f0c
Kevin <dev@kevinb.run>
2025-11-19 12:05:26 +0100

    change handi modalite

--- src/lib/components/formation.svelte
+++ src/lib/components/formation.svelte
@@ -1,86 +1,162 @@
 <script>
-    import Icon from "@iconify/svelte";
+	import Icon from '@iconify/svelte';
 </script>
 
 <section class=" my-8">
-    <div id="formation" class=" h-20"></div>
-    <h2 class=" text-2xl font-bold text-center">Formation</h2>
-    <div class=" flex flex-col flex-nowrap justify-start">
-        <details class=" border-b-2 border-base-300 py-2 px-1 gap-2 lg:hover:shadow-lg hover:cursor-pointer lg:w-5/12 lg:mx-auto">   
-            <summary class="  font-bold text-lg">Prérequis</summary>
-            <div>
-                <h3 class=" font-semibold">Prérequis d'entrée en formation: </h3>
-                    <ul class=" ml-2">
-                        <li class=" list-item"> - Disposer d’une expertise métier sur laquelle animer une séquence de formation.</li>
-                        <li class=" list-item"> - Une expérience professionnelle de 3 ans est souhaitée.</li>
-                    </ul>
-                <h3 class=" font-semibold">Prérequis techniques :</h3>
-                <p class=" ml-2">Disposer d’un ordinateur ou d’une tablette équipée d’une Webcam et avoir accès à réseau Internet haut débit pour les séances prévues à distance en Visio-conférence.</p>
-            </div>
-        </details>
-        <details class=" border-b-2 border-base-300 py-2 px-1 gap-2 lg:hover:shadow-lg hover:cursor-pointer lg:w-5/12 lg:mx-auto">   
-            <summary class="  font-bold text-lg">Objectifs pédagogiques</summary>
-            <div class=" pl-3 flex flex-col flex-nowrap gap-2 ">
-                <ul>
-                    <li> - Appréhender les techniques, les outils, les modalités et compétences nécessaires pour transmettre à un ou plusieurs apprenants un savoir ou un savoir-faire.</li>
-                    <li> - Gérer les situations sensibles avec agilité et sensibilité</li>
-                    <li> - Délivrer le bon contenu dans un bon timing</li>
-                    <li> - Conduire une action de formation en utilisant des techniques de communication (verbale ou non), de questionnement et d’écoute active</li>
-                    <li> - Créer des situations de feedback avec l’apprenant</li>
-                </ul>
-            </div>
-        </details>
-        <details class=" border-b-2 border-base-300 py-2 px-1 gap-2 lg:hover:shadow-lg hover:cursor-pointer lg:w-5/12 lg:mx-auto">   
-            <summary class="  font-bold text-lg">Public concerné</summary>
-            <div class=" pl-3 flex flex-col flex-nowrap gap-2 ">
-                <p>La certification s’adresse à des professionnels détenant des compétences spécifiques à leurs métiers, et sollicités afin de transmettre leurs pratiques à des collègues, collaborateurs ou clients dans le cadre d’animation ponctuelle de formation.</p>
-            </div>
-        </details>
-        <details class=" border-b-2 border-base-300 py-2 px-1 gap-2 lg:hover:shadow-lg hover:cursor-pointer lg:w-5/12 lg:mx-auto">   
-            <summary class="  font-bold text-lg">Durée de Formation</summary>
-            <div class=" flex flex-row flex-nowrap items-center justify-center font-bold">
-                <p class=" mr-2 text-accent">42</p> 
-                <Icon icon="svg-spinners:clock" />
-            </div>
-            <div class=" pl-3 flex flex-col flex-nowrap gap-2 ">
-                Cette formation se déroulera sur 6 semaines avec 2 sessions de 3h30 par semaine en comptant le travail personnel. 
-            </div>
-        </details>
-        <details class=" border-b-2 border-base-300 py-2 px-1 gap-2 lg:hover:shadow-lg hover:cursor-pointer lg:w-5/12 lg:mx-auto">   
-            <summary class="  font-bold text-lg">Modalités d'évaluation des acquis</summary>
-            <div class=" pl-3 flex flex-col flex-nowrap gap-2 ">
-                <ul>
-                    <li>A l’issue de la formation, une soutenance se fera devant un jury professionnel qui donnera lieu à une certification reconnue par France Compétences (répertoire spécifique).</li>
-                    <li>Les deux modes d’évaluation sont:</li>
-                    <li> - Soutenance orale</li>
-                    <li> - Simulation</li>
-                </ul>
-            </div>
-        </details>
-        <details class=" border-b-2 border-base-300 py-2 px-1 gap-2 lg:hover:shadow-lg hover:cursor-pointer lg:w-5/12 lg:mx-auto">   
-            <summary class="  font-bold text-lg">Moyens Pédagogiques</summary>
-            <div class=" pl-3 flex flex-col flex-nowrap gap-2 ">
-                <p>Connaissez-vous l’entraînement par la simulation évolutive ?</p>
-                <p>Elle est basée sur la représentation d’une réalité cohérente : par exemple vous endossez la fonction de formateur et notre formateur devient l’interlocuteur. Il vous confrontera en fonction de votre objectif.</p>
-                <p>Nous vous poussons à sortir de votre zone de confort et prendre conscience de votre posture d’animateur.</p>
-                <p>Ensuite, notre formateur fait un débriefing appelé « feed back » en identifiants vos points forts, vos axes d’amélioration et en élaborant un plan d’action personnalisé.</p>
-                <p>Notre formateur vous fait prendre conscience de votre propre potentiel et vous fait découvrir votre capacité à l’exploiter.</p>
-            </div>
-        </details>
-        <details class=" border-b-2 border-base-300 py-2 px-1 lg:hover:shadow-lg hover:cursor-pointer lg:w-5/12 lg:mx-auto">   
-            <summary class="  font-bold text-lg">Modalités, accessibilité et référent handicap</summary>
-            <div class=" pl-3 flex flex-col flex-nowrap gap-2 ">
-                <p>Accessibilité : si vous êtes en situation d'handicap, merci de le préciser lorsque vous recevrez le devis. Le référent handicap du centre de formation prendra alors contact avec vous pour s’assurer de l’accessibilité adéquate du dispositif de formation pour le week-end en présentiel .</p>
-                <p>Notre formation étant réalisée pour le reste en distanciel, les personnes à mobilité réduite peuvent y accéder de chez elles. Pour les autres types d’handicap merci de prendre <a href="#form" class=" font-semibold text-accent"> contact </a> avec nous.</p>
-            </div>
-        </details>
-        <details class=" border-b-2 border-base-300 py-2 px-1 lg:hover:shadow-lg hover:cursor-pointer lg:w-5/12 lg:mx-auto">   
-            <summary class="  font-bold text-lg">Certification</summary>
-            <div class=" pl-3 flex flex-col flex-nowrap gap-2 ">
-                <p>La certification : "Animer une formation en situation professionnelle", reconnue au RS vous sera délivrée en fin de formation après la réussite de votre soutenance face au jury</p>
-                <p>Certificateur: Magnitude</p>
-                <p>N° RS: 5846</p>
-            </div>
-        </details>
-    </div>
+	<div id="formation" class=" h-20"></div>
+	<h2 class=" text-2xl font-bold text-center">Formation</h2>
+	<div class=" flex flex-col flex-nowrap justify-start">
+		<details
+			class=" border-b-2 border-base-300 py-2 px-1 gap-2 lg:hover:shadow-lg hover:cursor-pointer lg:w-5/12 lg:mx-auto"
+		>
+			<summary class="  font-bold text-lg">Prérequis</summary>
+			<div>
+				<h3 class=" font-semibold">Prérequis d'entrée en formation:</h3>
+				<ul class=" ml-2">
+					<li class=" list-item">
+						- Disposer d’une expertise métier sur laquelle animer une séquence de formation.
+					</li>
+					<li class=" list-item">- Une expérience professionnelle de 3 ans est souhaitée.</li>
+				</ul>
+				<h3 class=" font-semibold">Prérequis techniques :</h3>
+				<p class=" ml-2">
+					Disposer d’un ordinateur ou d’une tablette équipée d’une Webcam et avoir accès à réseau
+					Internet haut débit pour les séances prévues à distance en Visio-conférence.
+				</p>
+			</div>
+		</details>
+		<details
+			class=" border-b-2 border-base-300 py-2 px-1 gap-2 lg:hover:shadow-lg hover:cursor-pointer lg:w-5/12 lg:mx-auto"
+		>
+			<summary class="  font-bold text-lg">Objectifs pédagogiques</summary>
+			<div class=" pl-3 flex flex-col flex-nowrap gap-2">
+				<ul>
+					<li>
+						- Appréhender les techniques, les outils, les modalités et compétences nécessaires pour
+						transmettre à un ou plusieurs apprenants un savoir ou un savoir-faire.
+					</li>
+					<li>- Gérer les situations sensibles avec agilité et sensibilité</li>
+					<li>- Délivrer le bon contenu dans un bon timing</li>
+					<li>
+						- Conduire une action de formation en utilisant des techniques de communication (verbale
+						ou non), de questionnement et d’écoute active
+					</li>
+					<li>- Créer des situations de feedback avec l’apprenant</li>
+				</ul>
+			</div>
+		</details>
+		<details
+			class=" border-b-2 border-base-300 py-2 px-1 gap-2 lg:hover:shadow-lg hover:cursor-pointer lg:w-5/12 lg:mx-auto"
+		>
+			<summary class="  font-bold text-lg">Public concerné</summary>
+			<div class=" pl-3 flex flex-col flex-nowrap gap-2">
+				<p>
+					La certification s’adresse à des professionnels détenant des compétences spécifiques à
+					leurs métiers, et sollicités afin de transmettre leurs pratiques à des collègues,
+					collaborateurs ou clients dans le cadre d’animation ponctuelle de formation.
+				</p>
+			</div>
+		</details>
+		<details
+			class=" border-b-2 border-base-300 py-2 px-1 gap-2 lg:hover:shadow-lg hover:cursor-pointer lg:w-5/12 lg:mx-auto"
+		>
+			<summary class="  font-bold text-lg">Durée de Formation</summary>
+			<div class=" flex flex-row flex-nowrap items-center justify-center font-bold">
+				<p class=" mr-2 text-accent">42</p>
+				<Icon icon="svg-spinners:clock" />
+			</div>
+			<div class=" pl-3 flex flex-col flex-nowrap gap-2">
+				Cette formation se déroulera sur 6 semaines avec 2 sessions de 3h30 par semaine en comptant
+				le travail personnel.
+			</div>
+		</details>
+		<details
+			class=" border-b-2 border-base-300 py-2 px-1 gap-2 lg:hover:shadow-lg hover:cursor-pointer lg:w-5/12 lg:mx-auto"
+		>
+			<summary class="  font-bold text-lg">Modalités d'évaluation des acquis</summary>
+			<div class=" pl-3 flex flex-col flex-nowrap gap-2">
+				<ul>
+					<li>
+						A l’issue de la formation, une soutenance se fera devant un jury professionnel qui
+						donnera lieu à une certification reconnue par France Compétences (répertoire
+						spécifique).
+					</li>
+					<li>Les deux modes d’évaluation sont:</li>
+					<li>- Soutenance orale</li>
+					<li>- Simulation</li>
+				</ul>
+			</div>
+		</details>
+		<details
+			class=" border-b-2 border-base-300 py-2 px-1 gap-2 lg:hover:shadow-lg hover:cursor-pointer lg:w-5/12 lg:mx-auto"
+		>
+			<summary class="  font-bold text-lg">Moyens Pédagogiques</summary>
+			<div class=" pl-3 flex flex-col flex-nowrap gap-2">
+				<p>Connaissez-vous l’entraînement par la simulation évolutive ?</p>
+				<p>
+					Elle est basée sur la représentation d’une réalité cohérente : par exemple vous endossez
+					la fonction de formateur et notre formateur devient l’interlocuteur. Il vous confrontera
+					en fonction de votre objectif.
+				</p>
+				<p>
+					Nous vous poussons à sortir de votre zone de confort et prendre conscience de votre
+					posture d’animateur.
+				</p>
+				<p>
+					Ensuite, notre formateur fait un débriefing appelé « feed back » en identifiants vos
+					points forts, vos axes d’amélioration et en élaborant un plan d’action personnalisé.
+				</p>
+				<p>
+					Notre formateur vous fait prendre conscience de votre propre potentiel et vous fait
+					découvrir votre capacité à l’exploiter.
+				</p>
+			</div>
+		</details>
+		<details
+			class=" border-b-2 border-base-300 py-2 px-1 lg:hover:shadow-lg hover:cursor-pointer lg:w-5/12 lg:mx-auto"
+		>
+			<summary class="  font-bold text-lg">Modalités, accessibilité et référent handicap</summary>
+			<div class=" pl-3 flex flex-col flex-nowrap gap-2">
+				<p>
+					Si vous êtes en situation de handicap (visible ou invisible), merci de nous le signaler
+					dès la demande de devis ou d’inscription.
+				</p>
+				<p>
+					Notre référent handicap, Ichem Brahami, vous proposera un échange individuel afin
+					d’identifier vos besoins et de vérifier les possibilités d’adaptation (supports, rythme,
+					modalités d’évaluation, organisation…).
+				</p>
+				<p>
+					La formation se déroulant majoritairement en distanciel, les personnes à mobilité réduite
+					peuvent y accéder depuis leur domicile. Pour le(s) week-end(s) en présentiel, nous
+					étudions au cas par cas les conditions d’accessibilité (locaux, stationnement, etc.).
+				</p>
+				<p>En fonction de votre situation, nous pouvons par exemple proposer :</p>
+				<ul class="list-disc">
+					<li>des supports en gros caractères ou en version numérique,</li>
+					<li>des consignes reformulées et découpées,</li>
+					<li>du temps supplémentaire pour certains exercices ou évaluations,</li>
+					<li>un aménagement de la place dans la salle ou des pauses plus fréquentes.</li>
+				</ul>
+				<p>
+					Pour toute question liée à un handicap, vous pouvez contacter directement le référent
+					handicap :
+				</p>
+				<a href="mailto=contact@ichemgame.fr" class="hover:text-success">contact@ichemgame.fr</a>
+			</div>
+		</details>
+		<details
+			class=" border-b-2 border-base-300 py-2 px-1 lg:hover:shadow-lg hover:cursor-pointer lg:w-5/12 lg:mx-auto"
+		>
+			<summary class="  font-bold text-lg">Certification</summary>
+			<div class=" pl-3 flex flex-col flex-nowrap gap-2">
+				<p>
+					La certification : "Animer une formation en situation professionnelle", reconnue au RS
+					vous sera délivrée en fin de formation après la réussite de votre soutenance face au jury
+				</p>
+				<p>Certificateur: Magnitude</p>
+				<p>N° RS: 5846</p>
+			</div>
+		</details>
+	</div>
 </section>
